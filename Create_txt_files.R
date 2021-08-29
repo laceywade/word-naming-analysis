0224d0dcaa5faba2eb4f4a260b77b0bda98127bf
laceywade <laceya@sas.upenn.edu>
2021-08-29 12:27:21 -0400

    Update Create_txt_files.R

--- Create_txt_files.R
+++ Create_txt_files.R
@@ -1,18 +1,23 @@
+### this script takes a csv file outputted by the Word Naming Game Task (run on PCIbexFarm) 
+### and creates individual txt files for each word elicited, to be used for Forced Alignment
+
 library(stringr)
 
-### Load in the "Cleaned_Results_date.csv" file from Ibex
+### Load in the csv file from Ibex
 ibex.full<-read.csv(file.choose(), header=T)
 ### Keep only lines with filenames
 ibex<-ibex.full[ibex.full$Parameter=="Filename",]
 ### Create filenames
 ibex$files<-str_replace(ibex$filename, ".wav", ".txt") #If your soundfiles are save as something other than .wav, adjust this here
 
-### Create tab-delimited transcript files
+### Create tab-delimited transcript files in working directory
 for (row in 1:nrow(ibex)) {
   file <- paste(ibex[row, "files"])
   id <- paste(ibex[row, "id"])
   word <- ibex[row, "word"]
   transcription <- paste("the word is", word)
-  contents <- data.frame(cbind(id, "0.1", "100000", transcription))
+### Choose whether you want txt files compatible with FAVE-Align or P2FA 
+  contents <- data.frame(cbind(id, "0.1", "100000", transcription)) # for use with FAVE-Align
+# contents <- transcription # uncomment for use with P2FA
    write.table(contents, file=file, sep="\t", col.names=FALSE, quote=FALSE)
   }
